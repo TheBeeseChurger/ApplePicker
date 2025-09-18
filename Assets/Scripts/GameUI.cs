2495f9170c496883dc6f4af31d71b9394ca05dc0
Jack Caesar <jackachulian@gmail.com>
2025-09-18 16:40:32 -0500

    Game UI canvas, Game UI script, Round counter and restart button

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class GameUI : MonoBehaviour
+    {
+        public Text roundText;
+        public int roundLength = 15;
+        
+        private int round = 0;
+        private float timeUntilNextRound = 0;
+        
+        private void FixedUpdate()
+        {
+            timeUntilNextRound -= Time.fixedDeltaTime;
+            if (timeUntilNextRound <= 0)
+            {
+                round++;
+                if (round > 4)
+                {
+                    // Win
+                }
+                else
+                {
+                    // First / Next Round
+                    roundText.text = "Round "+round;
+                    timeUntilNextRound = roundLength;
+                }
+            }
+        }
+
+        public void Restart()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
