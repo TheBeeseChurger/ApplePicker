6be988d60f7b1cac0b9c14afe790523e994c93d0
Brandon Underwood <147886188+TheBeeseChurger@users.noreply.github.com>
2025-09-18 16:08:21 -0500

    AppleScript & ProjectSettings Changes

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -17,11 +17,36 @@ public class AppleTree : MonoBehaviour
     
     void Start()
     {
-                
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
     }
 
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < changeDirChange)
+        {
+            speed *= -1;
+        }
     }
 }
