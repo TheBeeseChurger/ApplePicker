eafee181f1c1f02a4c80bebe727ad2126a7257c5
Brandon Underwood <147886188+TheBeeseChurger@users.noreply.github.com>
2025-09-17 19:48:12 -0500

    Initial commit

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float changeDirChange = 0.1f;
+
+    public float appleDropDelay = 1f;
+    
+    void Start()
+    {
+                
+    }
+
+    void Update()
+    {
+        
+    }
+}
